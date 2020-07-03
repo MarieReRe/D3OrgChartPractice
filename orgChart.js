4174cf746a58730a6ec74066ef90a485acc2ec5f
Marie Antons <chefantons@gmail.com>
2020-07-03 00:56:14 -0500

    failed attempt at adding text

--- orgChart.js
+++ orgChart.js
@@ -14,26 +14,54 @@ var chartGroup = svg.append('g')
 d3.json('/data/data.json').get(function(error, data){
    console.log(data);
    var root = d3.hierarchy(data, function(d){
-       console.log(d);
+
        return d.Entities || d.Assets;
+
    });
+
+   
    tree(root);
    console.log(root);
-   chartGroup.selectAll("circle")
+   chartGroup.selectAll(".node")
             //should be an array of nodes
             .data(root.descendants())
             .enter().append("circle")
                     .attr("cx",function(d){ return d.x;})
                     .attr("cy",function(d){ return d.y;})
-                    .attr("r","5");
+                    .attr("r","30")
+                    .attr("stroke", "black")
+                    .attr("fill", "blue")
+                    .append("text")
+                    .text(function(d) { return d.Name; }) ;
+                    
+        
+ 
+                   
+                 
+             
+     chartGroup.selectAll("path")
+                    .data(root.descendants().slice(1))
+                    .enter().append("path")
+                    .attr("stroke", "black")
+                    .attr("class", "link")
+                    .attr("d", function(d){
+               
+                      return "M"+d.parent.x+","+d.parent.y+"L"+d.x+","+d.y;});
+ 
 
 
-    chartGroup.selectAll("path")
-              .data(root.descendants().slice(1))
-              .enter().append("path")
-              .attr("stroke", "black")
-              .attr("class", "link")
-              .attr("d", function(d){
-                  console.log(d);
-                return "M"+d.parent.x+","+d.parent.y+"L"+d.x+","+d.y;});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 });
