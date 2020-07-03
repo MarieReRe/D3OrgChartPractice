db1146711d0ff11149b5c31e5ae77a7e9e02b0d0
Marie Antons <chefantons@gmail.com>
2020-07-02 20:28:31 -0500

    svg creation

--- orgChart.js
+++ orgChart.js
@@ -1,9 +1,9 @@
-d3.json("data.json", function(data) {
-    console.log(data);
-});
+var height = 200;
+var width = 500;
+var margin = {left: 50, right: 50, top: 40, bottom: 0};
+
+var tree = d3.tree().size([width,height]);
 
 var svg = d3.select("body").append("svg")
     .attr("height", "100%")
     .attr("width", "100%");
-
-
